1efb7cfa641a723e061ad02624dbb3082d366b7b
Javier Segovia Córdoba <javiersegoviacordoba@gmail.com>
2018-07-29 01:13:10 +0200

    Library finished
    TODO:
    
    - Readme
    - Wikis

--- src/main/java/Examples.java
+++ src/main/java/Examples.java
@@ -1,37 +1,5 @@
-import data_dragon.DataDragon;
-import data_dragon.constant.Locale;
-import data_dragon.constant.Platform;
-import data_dragon.endpoints.cdn.summoner_spell.SummonerSpellMethods;
-import data_dragon.endpoints.cdn.summoner_spell.dto.SummonerSpell;
-import data_dragon.utils.ErrorCode;
-
 public class Examples {
     public static void main(String[] args) {
 
-
-        DataDragon dataDragon = new DataDragon(Platform.NA);
-
-        dataDragon.getSummonerSpellAsync("SummonerDot", Locale.ES_ES, Platform.EUW.getVersion(),
-                new SummonerSpellMethods.SummonerSpellInterfaceAsync() {
-                    @Override
-                    public void onSuccess(SummonerSpell summonerSpell) {
-                        System.out.println(summonerSpell.toJson());
-                    }
-
-                    @Override
-                    public void onErrorCode(ErrorCode errorCode) {
-
-                    }
-
-                    @Override
-                    public void onNotFound() {
-
-                    }
-
-                    @Override
-                    public void onFailure(Throwable throwable) {
-
-                    }
-                });
     }
 }
